24848dd93c7dcf625c4b203c5e4ccc0ed63c8c69
Mobolanle Adebesin <bolaadebesin@gmail.com>
2022-12-10 11:28:01 -0500

    elevate state to app component, add reset button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
 import './App.css';
 import HitButton from './components/HitButton'
+
+import { React, useEffect, useState } from 'react';
+
 function App() {
+  const [count, setCount] = useState(null);
+
+  // Set Constants 
+  const API_KEY = process.env.REACT_APP_COUNT_KEY
+  const BASE_URL = process.env.REACT_APP_BASE_URL
+  const info = encodeURI(`${BASE_URL}/info/bolaadebesin/${API_KEY}`)
+  const hitUrl = encodeURI(`${BASE_URL}/hit/bolaadebesin/${API_KEY}`)
+  const resetUrl = encodeURI(`${BASE_URL}/set/bolaadebesin/${API_KEY}?value=0`)
+
+  // Mount Componentl with initial count/hit value
+  useEffect(() => {
+
+    fetch(info)
+      .then(res => {
+        if (res.ok) return res.json()
+        throw Error(`${res.status}: ${res.statusText}`)
+      })
+      .then(data => {
+        setCount(data.value)
+      })
+  }, [])
+
+
+  // onClick function fetches data from countapi
+  let onClick = async (e) => {
+    e.preventDefault();
+    let url
+    switch (e.target.name) {
+      case "hit":
+        url = hitUrl
+        break;
+      case "reset":
+        url = resetUrl
+        break;
+    }
+
+    let response = await fetch(url)
+    if (!response.ok) {
+      throw Error(`${response.status}: ${response.statusText}`)
+    }
+    let data = await response.json()
+    setCount(data.value)
+  }
+
   return (
     <div className="App">
-      <HitButton></HitButton>
+      <div>{count}</div>
+      <HitButton type="hit" onClick={(e) => onClick(e)}></HitButton>
+      <HitButton type="reset" onClick={(e) => onClick(e)}></HitButton>
+
     </div>
   );
 }
--- src/components/HitButton.js
+++ src/components/HitButton.js
@@ -3,42 +3,9 @@ import { React, useEffect, useState } from 'react';
 import "./HitButton.css"
 
 const HitButton = (props) => {
-    // Initialize State
-    const [count, setCount] = useState(null);
-
-    // Set Constants 
-    const API_KEY = process.env.REACT_APP_COUNT_KEY
-    const BASE_URL = process.env.REACT_APP_BASE_URL
-    const info = encodeURI(`${BASE_URL}/info/bolaadebesin/${API_KEY}`)
-    const url = encodeURI(`${BASE_URL}/hit/bolaadebesin/${API_KEY}`)
-
-    // Mount Componentl with initial count/hit value
-    useEffect(() => {
-
-        fetch(info)
-            .then(res => {
-                if (res.ok) return res.json()
-                throw Error(`${res.status}: ${res.statusText}`)
-            })
-            .then(data => {
-                setCount(data.value)
-            })
-    }, [])
-
-    let onClick = async (e) => {
-        e.preventDefault();
-        let response = await fetch(url)
-        if (!response.ok) {
-            throw Error(`${response.status}: ${response.statusText}`)
-        }
-        let data = await response.json()
-        setCount(data.value)
-    }
-
     return (
         <div className="HitButton-Container">
-            <div>{count}</div>
-            <button className="HitButton-Button" onClick={onClick}>Click Me</button>
+            <button className="HitButton-Button" name={props.type} onClick={props.onClick}>{props.type === "hit" ? "Click Me" : "Reset"}</button>
         </div>
     )
 }
